178561538d8abc43faa9e14283f28de0805cb780
creator <creator@bash.org.ru>
2013-01-14 21:29:14 +0300

    overseer new app new stream output

--- src/cocaine_node_app_info.cpp
+++ src/cocaine_node_app_info.cpp
@@ -62,10 +62,19 @@ std::ostream& operator << (std::ostream& out, const cocaine_node_app_info_t& inf
 	out << "status: " << std::setw(0) << status << std::endl;
 
 	out << std::setw(2) << "" << std::setw(4) << "" << std::setw(9);
+	out << "profile: " << std::setw(0) << info.profile << std::endl;
+
+	out << std::setw(2) << "" << std::setw(4) << "" << std::setw(9);
+	out << "load median: " << std::setw(0) << info.load_median << std::endl;
+
+	out << std::setw(2) << "" << std::setw(4) << "" << std::setw(9);
 	out << "slaves: " << std::setw(0) << slaves.str() << std::endl;
 
 	out << std::setw(2) << "" << std::setw(4) << "" << std::setw(9);
-	out << "queue: " << std::setw(0) << info.queue_depth << std::endl;
+	out << "sessions pending: " << std::setw(0) << info.sessions_pending << std::endl;
+
+	out << std::setw(2) << "" << std::setw(4) << "" << std::setw(9);
+	out << "queue depth: " << std::setw(0) << info.queue_depth << std::endl;
 	
 	out << std::setw(2) << "" << std::setw(4) << "" << std::setw(9);
 	out << "tasks:" << std::endl;
