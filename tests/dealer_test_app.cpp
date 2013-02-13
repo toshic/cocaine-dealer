810f13a332e755b446063607143a51be3adc8e50
tinybit <creator@bash.org.ru>
2013-02-13 17:26:50 +0300

    updated tests

--- tests/dealer_test_app.cpp
+++ tests/dealer_test_app.cpp
@@ -86,8 +86,6 @@ void worker(dealer_t* d,
 void create_client(size_t dealers_count, size_t threads_per_dealer, size_t messages_count) {
 	std::string config_path = "../tests/config.json";
 
-
-
 	typedef boost::ptr_vector<boost::thread> thread_pool;
 	typedef boost::ptr_vector<thread_pool> thread_pools_list;
 
@@ -142,7 +140,7 @@ void create_client(size_t dealers_count, size_t threads_per_dealer, size_t messa
 int
 main(int argc, char** argv) {
 	dealer_t			d("../tests/config.json");
-	message_path_t		path("time_echo", "echo");
+	message_path_t		path("dummy", "dummy");
 	std::string			payload = "message ";
 
 	boost::shared_ptr<response_t> responce;
